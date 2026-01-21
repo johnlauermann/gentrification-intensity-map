9a3038375a6c791b9162a238a969f1c451fc8496
johnlauermann <72218401+johnlauermann@users.noreply.github.com>
2026-01-21 11:54:34 -0500

    new function to streamline calculations

--- 01_data/data_combineallyears_2020tr.R
+++ 01_data/data_combineallyears_2020tr.R
@@ -15,7 +15,7 @@ library(sf)
 library(stringr)
 
 #set working directory & source data
-here::i_am("data_combine_allyears_2020tr.r")
+here::i_am("01_data/data_combineallyears_2020tr.r")
 data1990 <- read.csv("tractdata_1990_2020tr.csv")
 data2000 <- read.csv("tractdata_2000_2020tr.csv")
 data2010 <- read.csv("tractdata_2010_2020tr.csv")
@@ -31,182 +31,49 @@ data <- data2020 %>%
 data <- data %>% select(tr2020gj, sort(setdiff(names(.), "tr2020gj")))
 
 
-#calculate change variables
-##for 1990 to 2000
-data <- within(data, {
-  Adults_sum_chg_1990to2000 <- Adults_sum_2000 - Adults_sum_1990
-  Asian_pct_chg_1990to2000 <- Asian_pct_2000 - Asian_pct_1990
-  Asian_sum_chg_1990to2000 <- Asian_sum_2000 - Asian_sum_1990
-  Bach_pct_chg_1990to2000 <- Bach_pct_2000 - Bach_pct_1990
-  Bach_sum_chg_1990to2000 <- Bach_sum_2000 - Bach_sum_1990
-  Black_pct_chg_1990to2000 <- Black_pct_2000 - Black_pct_1990
-  Black_sum_chg_1990to2000 <- Black_sum_2000 - Black_sum_1990
-  ConRent_agg_chg_1990to2000 <- ConRent_agg_2000 - ConRent_agg_1990
-  ConRent_mean_chg_1990to2000 <- ConRent_mean_2000 - ConRent_mean_1990
-  Elocal_chg_1990to2000 <- Elocal_2000 - Elocal_1990
-  Employed_sum_chg_1990to2000 <- Employed_sum_2000 - Employed_sum_1990
-  HHIncome_agg_chg_1990to2000 <- HHIncome_agg_2000 - HHIncome_agg_1990
-  HHIncome_mean_chg_1990to2000 <- HHIncome_mean_2000 - HHIncome_mean_1990
-  Hispanic_pct_chg_1990to2000 <- Hispanic_pct_2000 - Hispanic_pct_1990
-  Hispanic_sum_chg_1990to2000 <- Hispanic_sum_2000 - Hispanic_sum_1990
-  Households_sum_chg_1990to2000 <- Households_sum_2000 - Households_sum_1990
-  HouseUnits_sum_chg_1990to2000 <- HouseUnits_sum_2000 - HouseUnits_sum_1990
-  HouseValue_agg_chg_1990to2000 <- HouseValue_agg_2000 - HouseValue_agg_1990
-  HouseValue_mean_chg_1990to2000 <- HouseValue_mean_2000 - HouseValue_mean_1990
-  HouseValue_Mortgaged_agg_chg_1990to2000 <- HouseValue_Mortgaged_agg_2000 - HouseValue_Mortgaged_agg_1990
-  HouseValue_Mortgaged_mean_chg_1990to2000 <- HouseValue_Mortgaged_mean_2000 - HouseValue_Mortgaged_mean_1990
-  HUOccupied_sum_chg_1990to2000 <- HUOccupied_sum_2000 - HUOccupied_sum_1990
-  HUOwner_pct_chg_1990to2000 <- HUOwner_pct_2000 - HUOwner_pct_1990
-  HUOwner_sum_chg_1990to2000 <- HUOwner_sum_2000 - HUOwner_sum_1990
-  HURenter_pct_chg_1990to2000 <- HURenter_pct_2000 - HURenter_pct_1990
-  HURenter_sum_chg_1990to2000 <- HURenter_sum_2000 - HURenter_sum_1990
-  HUVacant_pct_chg_1990to2000 <- HUVacant_pct_2000 - HUVacant_pct_1990
-  HUVacant_sum_chg_1990to2000 <- HUVacant_sum_2000 - HUVacant_sum_1990
-  MovedIn_under10yrs_pct_chg_1990to2000 <- MovedIn_under10yrs_pct_2000 - MovedIn_under10yrs_pct_1990
-  MovedIn_under10yrs_sum_chg_1990to2000 <- MovedIn_under10yrs_sum_2000 - MovedIn_under10yrs_sum_1990
-  Other_pct_chg_1990to2000 <- Other_pct_2000 - Other_pct_1990
-  Other_sum_chg_1990to2000 <- Other_sum_2000 - Other_sum_1990
-  Population_sum_chg_1990to2000 <- Population_sum_2000 - Population_sum_1990
-  Poverty_pct_chg_1990to2000 <- Poverty_pct_2000 - Poverty_pct_1990
-  Poverty_sum_chg_1990to2000 <- Poverty_sum_2000 - Poverty_sum_1990
-  White_pct_chg_1990to2000 <- White_pct_2000 - White_pct_1990
-  White_sum_chg_1990to2000 <- White_sum_2000 - White_sum_1990
-  WhiteCollar_pct_chg_1990to2000 <- WhiteCollar_pct_2000 - WhiteCollar_pct_1990
-  WhiteCollar_sum_chg_1990to2000 <- WhiteCollar_sum_2000 - WhiteCollar_sum_1990
-})
 
-##for 2000 to 2010
-data <- within(data, {
-  Adults_sum_chg_2000to2010 <- Adults_sum_2010 - Adults_sum_2000
-  Asian_pct_chg_2000to2010 <- Asian_pct_2010 - Asian_pct_2000
-  Asian_sum_chg_2000to2010 <- Asian_sum_2010 - Asian_sum_2000
-  Bach_pct_chg_2000to2010 <- Bach_pct_2010 - Bach_pct_2000
-  Bach_sum_chg_2000to2010 <- Bach_sum_2010 - Bach_sum_2000
-  Black_pct_chg_2000to2010 <- Black_pct_2010 - Black_pct_2000
-  Black_sum_chg_2000to2010 <- Black_sum_2010 - Black_sum_2000
-  ConRent_agg_chg_2000to2010 <- ConRent_agg_2010 - ConRent_agg_2000
-  ConRent_mean_chg_2000to2010 <- ConRent_mean_2010 - ConRent_mean_2000
-  Elocal_chg_2000to2010 <- Elocal_2010 - Elocal_2000
-  Employed_sum_chg_2000to2010 <- Employed_sum_2010 - Employed_sum_2000
-  HHIncome_agg_chg_2000to2010 <- HHIncome_agg_2010 - HHIncome_agg_2000
-  HHIncome_mean_chg_2000to2010 <- HHIncome_mean_2010 - HHIncome_mean_2000
-  Hispanic_pct_chg_2000to2010 <- Hispanic_pct_2010 - Hispanic_pct_2000
-  Hispanic_sum_chg_2000to2010 <- Hispanic_sum_2010 - Hispanic_sum_2000
-  Households_sum_chg_2000to2010 <- Households_sum_2010 - Households_sum_2000
-  HouseUnits_sum_chg_2000to2010 <- HouseUnits_sum_2010 - HouseUnits_sum_2000
-  HouseValue_agg_chg_2000to2010 <- HouseValue_agg_2010 - HouseValue_agg_2000
-  HouseValue_mean_chg_2000to2010 <- HouseValue_mean_2010 - HouseValue_mean_2000
-  HouseValue_Mortgaged_agg_chg_2000to2010 <- HouseValue_Mortgaged_agg_2010 - HouseValue_Mortgaged_agg_2000
-  HouseValue_Mortgaged_mean_chg_2000to2010 <- HouseValue_Mortgaged_mean_2010 - HouseValue_Mortgaged_mean_2000
-  HUOccupied_sum_chg_2000to2010 <- HUOccupied_sum_2010 - HUOccupied_sum_2000
-  HUOwner_pct_chg_2000to2010 <- HUOwner_pct_2010 - HUOwner_pct_2000
-  HUOwner_sum_chg_2000to2010 <- HUOwner_sum_2010 - HUOwner_sum_2000
-  HURenter_pct_chg_2000to2010 <- HURenter_pct_2010 - HURenter_pct_2000
-  HURenter_sum_chg_2000to2010 <- HURenter_sum_2010 - HURenter_sum_2000
-  HUVacant_pct_chg_2000to2010 <- HUVacant_pct_2010 - HUVacant_pct_2000
-  HUVacant_sum_chg_2000to2010 <- HUVacant_sum_2010 - HUVacant_sum_2000
-  MovedIn_under10yrs_pct_chg_2000to2010 <- MovedIn_under10yrs_pct_2010 - MovedIn_under10yrs_pct_2000
-  MovedIn_under10yrs_sum_chg_2000to2010 <- MovedIn_under10yrs_sum_2010 - MovedIn_under10yrs_sum_2000
-  Multi_pct_chg_2000to2010 <- Multi_pct_2010 - Multi_pct_2000
-  Multi_sum_chg_2000to2010 <- Multi_sum_2010 - Multi_sum_2000
-  Other_pct_chg_2000to2010 <- Other_pct_2010 - Other_pct_2000
-  Other_sum_chg_2000to2010 <- Other_sum_2010 - Other_sum_2000
-  Population_sum_chg_2000to2010 <- Population_sum_2010 - Population_sum_2000
-  Poverty_pct_chg_2000to2010 <- Poverty_pct_2010 - Poverty_pct_2000
-  Poverty_sum_chg_2000to2010 <- Poverty_sum_2010 - Poverty_sum_2000
-  White_pct_chg_2000to2010 <- White_pct_2010 - White_pct_2000
-  White_sum_chg_2000to2010 <- White_sum_2010 - White_sum_2000
-  WhiteCollar_pct_chg_2000to2010 <- WhiteCollar_pct_2010 - WhiteCollar_pct_2000
-  WhiteCollar_sum_chg_2000to2010 <- WhiteCollar_sum_2010 - WhiteCollar_sum_2000
-})
+#calculate rates of change
 
-##for 2010 to 2020
-data <- within(data, {
-  Adults_sum_chg_2010to2020 <- Adults_sum_2020 - Adults_sum_2010
-  Asian_pct_chg_2010to2020 <- Asian_pct_2020 - Asian_pct_2010
-  Asian_sum_chg_2010to2020 <- Asian_sum_2020 - Asian_sum_2010
-  Bach_pct_chg_2010to2020 <- Bach_pct_2020 - Bach_pct_2010
-  Bach_sum_chg_2010to2020 <- Bach_sum_2020 - Bach_sum_2010
-  Black_pct_chg_2010to2020 <- Black_pct_2020 - Black_pct_2010
-  Black_sum_chg_2010to2020 <- Black_sum_2020 - Black_sum_2010
-  ConRent_agg_chg_2010to2020 <- ConRent_agg_2020 - ConRent_agg_2010
-  ConRent_mean_chg_2010to2020 <- ConRent_mean_2020 - ConRent_mean_2010
-  Elocal_chg_2010to2020 <- Elocal_2020 - Elocal_2010
-  Employed_sum_chg_2010to2020 <- Employed_sum_2020 - Employed_sum_2010
-  HHIncome_agg_chg_2010to2020 <- HHIncome_agg_2020 - HHIncome_agg_2010
-  HHIncome_mean_chg_2010to2020 <- HHIncome_mean_2020 - HHIncome_mean_2010
-  Hispanic_pct_chg_2010to2020 <- Hispanic_pct_2020 - Hispanic_pct_2010
-  Hispanic_sum_chg_2010to2020 <- Hispanic_sum_2020 - Hispanic_sum_2010
-  Households_sum_chg_2010to2020 <- Households_sum_2020 - Households_sum_2010
-  HouseUnits_sum_chg_2010to2020 <- HouseUnits_sum_2020 - HouseUnits_sum_2010
-  HouseValue_agg_chg_2010to2020 <- HouseValue_agg_2020 - HouseValue_agg_2010
-  HouseValue_mean_chg_2010to2020 <- HouseValue_mean_2020 - HouseValue_mean_2010
-  HouseValue_Mortgaged_agg_chg_2010to2020 <- HouseValue_Mortgaged_agg_2020 - HouseValue_Mortgaged_agg_2010
-  HouseValue_Mortgaged_mean_chg_2010to2020 <- HouseValue_Mortgaged_mean_2020 - HouseValue_Mortgaged_mean_2010
-  HUOccupied_sum_chg_2010to2020 <- HUOccupied_sum_2020 - HUOccupied_sum_2010
-  HUOwner_pct_chg_2010to2020 <- HUOwner_pct_2020 - HUOwner_pct_2010
-  HUOwner_sum_chg_2010to2020 <- HUOwner_sum_2020 - HUOwner_sum_2010
-  HURenter_pct_chg_2010to2020 <- HURenter_pct_2020 - HURenter_pct_2010
-  HURenter_sum_chg_2010to2020 <- HURenter_sum_2020 - HURenter_sum_2010
-  HUVacant_pct_chg_2010to2020 <- HUVacant_pct_2020 - HUVacant_pct_2010
-  HUVacant_sum_chg_2010to2020 <- HUVacant_sum_2020 - HUVacant_sum_2010
-  MovedIn_under10yrs_pct_chg_2010to2020 <- MovedIn_under10yrs_pct_2020 - MovedIn_under10yrs_pct_2010
-  MovedIn_under10yrs_sum_chg_2010to2020 <- MovedIn_under10yrs_sum_2020 - MovedIn_under10yrs_sum_2010
-  Multi_pct_chg_2010to2020 <- Multi_pct_2020 - Multi_pct_2010
-  Multi_sum_chg_2010to2020 <- Multi_sum_2020 - Multi_sum_2010
-  Other_pct_chg_2010to2020 <- Other_pct_2020 - Other_pct_2010
-  Other_sum_chg_2010to2020 <- Other_sum_2020 - Other_sum_2010
-  Population_sum_chg_2010to2020 <- Population_sum_2020 - Population_sum_2010
-  Poverty_pct_chg_2010to2020 <- Poverty_pct_2020 - Poverty_pct_2010
-  Poverty_sum_chg_2010to2020 <- Poverty_sum_2020 - Poverty_sum_2010
-  White_pct_chg_2010to2020 <- White_pct_2020 - White_pct_2010
-  White_sum_chg_2010to2020 <- White_sum_2020 - White_sum_2010
-  WhiteCollar_pct_chg_2010to2020 <- WhiteCollar_pct_2020 - WhiteCollar_pct_2010
-  WhiteCollar_sum_chg_2010to2020 <- WhiteCollar_sum_2020 - WhiteCollar_sum_2010
-})
+## function for calculating decadal change
+decadal_change <- function(data, variables, year1, year2) {
+  for (v in variables) {
+    v_year1 <- paste0(v, "_", year1)
+    v_year2 <- paste0(v, "_", year2)
+    v_change <- paste0(v, "_chg_", year1, "to", year2)
+    
+    if (!all(c(v_year1, v_year2) %in% names(data))) {
+      warning(paste("Skipped", v, "because data are missing."))
+      next
+    }
+    
+    data[[v_change]] <- data[[v_year2]] - data[[v_year1]]
+  }
+  
+  return(data)
+}
+  
+## list of variables
+variable_list <- c(
+  "Adults_sum", "Asian_pct", "Asian_sum", "Bach_pct", "Bach_sum",
+  "Black_pct", "Black_sum", "ConRent_agg", "ConRent_mean", "Elocal",
+  "Employed_sum", "HHIncome_agg", "HHIncome_mean", "Hispanic_pct",
+  "Hispanic_sum", "Households_sum", "HouseUnits_sum", "HouseValue_agg",
+  "HouseValue_mean", "HouseValue_Mortgaged_agg", "HouseValue_Mortgaged_mean",
+  "HUOccupied_sum", "HUOwner_pct", "HUOwner_sum", "HURenter_pct",
+  "HURenter_sum", "HUVacant_pct", "HUVacant_sum",
+  "MovedIn_under10yrs_pct", "MovedIn_under10yrs_sum", "Multi_pct", "Multi_sum",
+  "Other_pct", "Other_sum", "Population_sum", "Poverty_pct",
+  "Poverty_sum", "White_pct", "White_sum", "WhiteCollar_pct",
+  "WhiteCollar_sum")
 
-##for the entire range, 1990 to 2020
-data <- within(data, {
-  Adults_sum_chg_1990to2020 <- Adults_sum_2020 - Adults_sum_1990
-  Asian_pct_chg_1990to2020 <- Asian_pct_2020 - Asian_pct_1990
-  Asian_sum_chg_1990to2020 <- Asian_sum_2020 - Asian_sum_1990
-  Bach_pct_chg_1990to2020 <- Bach_pct_2020 - Bach_pct_1990
-  Bach_sum_chg_1990to2020 <- Bach_sum_2020 - Bach_sum_1990
-  Black_pct_chg_1990to2020 <- Black_pct_2020 - Black_pct_1990
-  Black_sum_chg_1990to2020 <- Black_sum_2020 - Black_sum_1990
-  ConRent_agg_chg_1990to2020 <- ConRent_agg_2020 - ConRent_agg_1990
-  ConRent_mean_chg_1990to2020 <- ConRent_mean_2020 - ConRent_mean_1990
-  Elocal_chg_1990to2020 <- Elocal_2020 - Elocal_1990
-  Employed_sum_chg_1990to2020 <- Employed_sum_2020 - Employed_sum_1990
-  HHIncome_agg_chg_1990to2020 <- HHIncome_agg_2020 - HHIncome_agg_1990
-  HHIncome_mean_chg_1990to2020 <- HHIncome_mean_2020 - HHIncome_mean_1990
-  Hispanic_pct_chg_1990to2020 <- Hispanic_pct_2020 - Hispanic_pct_1990
-  Hispanic_sum_chg_1990to2020 <- Hispanic_sum_2020 - Hispanic_sum_1990
-  Households_sum_chg_1990to2020 <- Households_sum_2020 - Households_sum_1990
-  HouseUnits_sum_chg_1990to2020 <- HouseUnits_sum_2020 - HouseUnits_sum_1990
-  HouseValue_agg_chg_1990to2020 <- HouseValue_agg_2020 - HouseValue_agg_1990
-  HouseValue_mean_chg_1990to2020 <- HouseValue_mean_2020 - HouseValue_mean_1990
-  HouseValue_Mortgaged_agg_chg_1990to2020 <- HouseValue_Mortgaged_agg_2020 - HouseValue_Mortgaged_agg_1990
-  HouseValue_Mortgaged_mean_chg_1990to2020 <- HouseValue_Mortgaged_mean_2020 - HouseValue_Mortgaged_mean_1990
-  HUOccupied_sum_chg_1990to2020 <- HUOccupied_sum_2020 - HUOccupied_sum_1990
-  HUOwner_pct_chg_1990to2020 <- HUOwner_pct_2020 - HUOwner_pct_1990
-  HUOwner_sum_chg_1990to2020 <- HUOwner_sum_2020 - HUOwner_sum_1990
-  HURenter_pct_chg_1990to2020 <- HURenter_pct_2020 - HURenter_pct_1990
-  HURenter_sum_chg_1990to2020 <- HURenter_sum_2020 - HURenter_sum_1990
-  HUVacant_pct_chg_1990to2020 <- HUVacant_pct_2020 - HUVacant_pct_1990
-  HUVacant_sum_chg_1990to2020 <- HUVacant_sum_2020 - HUVacant_sum_1990
-  MovedIn_under10yrs_pct_chg_1990to2020 <- MovedIn_under10yrs_pct_2020 - MovedIn_under10yrs_pct_1990
-  MovedIn_under10yrs_sum_chg_1990to2020 <- MovedIn_under10yrs_sum_2020 - MovedIn_under10yrs_sum_1990
-  Other_pct_chg_1990to2020 <- Other_pct_2020 - Other_pct_1990
-  Other_sum_chg_1990to2020 <- Other_sum_2020 - Other_sum_1990
-  Population_sum_chg_1990to2020 <- Population_sum_2020 - Population_sum_1990
-  Poverty_pct_chg_1990to2020 <- Poverty_pct_2020 - Poverty_pct_1990
-  Poverty_sum_chg_1990to2020 <- Poverty_sum_2020 - Poverty_sum_1990
-  White_pct_chg_1990to2020 <- White_pct_2020 - White_pct_1990
-  White_sum_chg_1990to2020 <- White_sum_2020 - White_sum_1990
-  WhiteCollar_pct_chg_1990to2020 <- WhiteCollar_pct_2020 - WhiteCollar_pct_1990
-  WhiteCollar_sum_chg_1990to2020 <- WhiteCollar_sum_2020 - WhiteCollar_sum_1990
-})
+
+## calculate
+data <- decadal_change(data = data, variables = variable_list, year1 = 1990, year2 = 2000)
+data <- decadal_change(data = data, variables = variable_list, year1 = 2000, year2 = 2010)
+data <- decadal_change(data = data, variables = variable_list, year1 = 2010, year2 = 2020)
+data <- decadal_change(data = data, variables = variable_list, year1 = 1990, year2 = 2020)
+
+
 
 #verify the shape of the data
 ## Create an empty dataframe
@@ -250,16 +117,10 @@ data <- data %>% select(tr2020gj, sort(setdiff(names(.), "tr2020gj")))
 write.csv(data, file = "tractdata_allyears_2020tr.csv", na="", row.names = FALSE)
 
 ##save a sample of 'urban' census tracts
-### load geographic boundaries
-st_layers("tract_boundaries.gpkg")
-boundaries <- st_read("tract_boundaries.gpkg", layer = "metrotracts_boundaries_2020tr")
-
-### merge
+boundaries <- read.csv(here("03_spatial/metrotracts_2020tr.csv"))
 data <- boundaries %>%
   left_join(data, by = c("GISJOIN" = "tr2020gj")) %>%
   select(-ends_with(".y")) %>% 
   rename_with(~ sub("\\.x$", "", .x), .cols = ends_with(".x"))
 
-### remove spatial formatting and save
-data <- st_drop_geometry(data)
 write.csv(data, file = "metrotracts_data_2020tr.csv", na="", row.names = FALSE)
